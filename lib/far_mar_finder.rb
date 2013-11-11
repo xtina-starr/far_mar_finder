8e71f11d17fbbe6a6484b53cda42d3eebe43619d
Davida Marion <davidamarion@gmail.com>
2013-11-11 15:14:26 -0800

    adding requirements

--- lib/far_mar_finder.rb
+++ lib/far_mar_finder.rb
@@ -2,6 +2,9 @@ require 'csv'
 require 'time'
 # require "./lib/marker" this is the same thing as line 4
 require_relative 'market'
+require_relative 'sale'
+require_relative 'product'
+require_relative 'vendor'
 # ... Require all of the supporting classes
 
 class FarMarFinder
